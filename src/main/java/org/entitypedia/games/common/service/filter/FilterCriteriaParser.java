e8bece46c78a05e1eb026988f2fd8de869486a15
autayeu <autayeu@468f3c8c-1912-47de-8909-53fc7abe8733>
2013-04-09 21:19:00 +0000

    order support in listLayouts, listCrosswords, listGames
    git-svn-id: https://dev.entitypedia.org/svn/entitypedia/apps/word-games-common/trunk@1434 468f3c8c-1912-47de-8909-53fc7abe8733

--- src/main/java/org/entitypedia/games/common/service/filter/FilterCriteriaParser.java
+++ src/main/java/org/entitypedia/games/common/service/filter/FilterCriteriaParser.java
@@ -1,13 +1,14 @@
 package org.entitypedia.games.common.service.filter;
 
 import org.antlr.v4.runtime.ANTLRErrorListener;
-import org.antlr.v4.runtime.ANTLRErrorStrategy;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.entitypedia.games.common.exceptions.OrderParsingException;
 import org.entitypedia.games.common.service.filter.parser.FilterLexer;
 import org.entitypedia.games.common.service.filter.parser.FilterParser;
 import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
 
 /**
  * Parses filter expressions for Hibernate Criteria. Example:
@@ -35,6 +36,7 @@ import org.hibernate.criterion.Criterion;
 public class FilterCriteriaParser {
 
     private static final ANTLRErrorListener errorListener = new ThrowingErrorListener();
+    private static final Order[] EMPTY_ORDER = new Order[0];
 
     public static Criterion parse(String filter) {
         ANTLRInputStream input = new ANTLRInputStream(unescape(filter));
@@ -48,6 +50,41 @@ public class FilterCriteriaParser {
         ParseTree parseTree = parser.init();
         FilterCriterionVisitor filterCriterionVisitor = new FilterCriterionVisitor();
         return filterCriterionVisitor.visit(parseTree);
+    }
+
+    public static Order[] order(String order) {
+        if (null == order) {
+            return EMPTY_ORDER;
+        }
+        String[] orders = order.split("-");
+        if (0 == order.length()) {
+            throw new OrderParsingException("Order expression is empty");
+        }
+        Order[] result = new Order[orders.length];
+        for (int i = 0; i < orders.length; i++) {
+            String o = orders[i];
+            if (o.length() < 2) {
+                throw new OrderParsingException("Order expression part too short: " + o);
+            }
+            if (o.charAt(0) == 'D' || o.charAt(0) == 'A') {
+                switch (o.charAt(0)) {
+                    case 'D': {
+                        result[i] = Order.desc(o.substring(1));
+                        break;
+                    }
+                    case 'A': {
+                        result[i] = Order.asc(o.substring(1));
+                        break;
+                    }
+                    default: {
+                        throw new OrderParsingException("Order expression parts should start with D or A: " + o);
+                    }
+                }
+            } else {
+                throw new OrderParsingException("Order expression parts should start with D or A: " + o);
+            }
+        }
+        return result;
     }
 
     private static String unescape(String filter) {
