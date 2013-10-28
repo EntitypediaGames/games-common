faf2c22f51bd08164f898ede66126afca385724a
autayeu <autayeu@468f3c8c-1912-47de-8909-53fc7abe8733>
2013-10-28 22:35:59 +0000

    TestSuite added
    git-svn-id: https://dev.entitypedia.org/svn/entitypedia/apps/word-games-common/trunk@1957 468f3c8c-1912-47de-8909-53fc7abe8733

---
+++ src/test/java/org/entitypedia/games/common/TestSuite.java
@@ -0,0 +1,21 @@
+package org.entitypedia.games.common;
+
+import org.entitypedia.games.common.repository.hibernateimpl.filter.TestFilterCriteriaParser;
+import org.entitypedia.games.common.tries.TestBasicTrie;
+import org.entitypedia.games.common.tries.TestBasicTrieNode;
+import org.entitypedia.games.common.tries.TestPackedTrie;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author <a rel="author" href="http://autayeu.com/">Aliaksandr Autayeu</a>
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestFilterCriteriaParser.class,
+        TestBasicTrie.class,
+        TestBasicTrieNode.class,
+        TestPackedTrie.class
+})
+public class TestSuite {
+}
